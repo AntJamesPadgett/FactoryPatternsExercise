bc44e5c02b5d17f922390834826c0fbe188492e1
antjamespadgett <padgettanthonyj@gmail.com>
2022-01-14 12:59:37 -0600

    Completed the exercise

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FactoryPatternsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfWheels;
+            bool input = false;
+            do
+            {
+                Console.WriteLine("Enter the amount of tires for the vehicle we wan to create:");
+
+                
+
+                 input = int.TryParse(Console.ReadLine(), out numOfWheels );
+
+            } while (!input);
+
+            var vehicle = VehicleFactory.GetVehicle(numOfWheels);
+            vehicle.Drive();
+        }
+    }
+}
---
+++ ThreeWheeler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternsExercise
+{
+     public class ThreeWheeler : IVehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Driving up the mountains!!!");
+        }
+    }
+}
